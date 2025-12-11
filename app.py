4f5106e5115013529a3d8382b938bc991c3d79e4
Sophie Kim <sek2dcs@virginia.edu>
2025-12-11 16:09:39 -0500

    fixing it hopeflly

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from dash import Dash, dcc, html, Input, Output, callback, dash_table
 import dash_bootstrap_components as dbc
-import dash_mantine_components as dmc
 from dash_iconify import DashIconify
 import pandas as pd
 import numpy as np
